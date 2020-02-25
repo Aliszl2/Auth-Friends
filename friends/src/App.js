7b522a82192f2acd80786decb3107bac031980f1
Alison Law <alisparklaw@gmail.com>
2020-02-25 12:43:06 +0000

    token authentication, login and get request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import { Route, NavLink } from "react-router-dom";
+import React from "react";
+import { Route, NavLink, Redirect } from "react-router-dom";
 import Friends from "./components/Friends";
 import Login from "./components/Login";
 import AddFriendForm from "./components/AddFriend";
 
-
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
+  const onLogout = (props) => {
+    localStorage.removeItem("token");
+    //neither work:
+    // props.history.push("/login");
+    // < Redirect to= "/login"/>
+  };
   return (
     <div className="App">
-      
-          Friends <br/>
-          <NavLink to="/friends">Friends</NavLink>&nbsp;&nbsp;&nbsp;
-          <NavLink to="/addfriend">Add a Friend</NavLink>&nbsp;&nbsp;&nbsp;
-          <NavLink to="/login">Login</NavLink>
-          <Route path="/friends" component={Friends} />
-          <Route path="/login" component={Login} />
-          <Route path="/addfriend" component={AddFriendForm } />
-
-         </div>
-     
-
+      Friends <br />
+      <NavLink to="/friends">Friends</NavLink>&nbsp;&nbsp;&nbsp;
+      <NavLink to="/addfriend">Add a Friend</NavLink>&nbsp;&nbsp;&nbsp;
+      <NavLink to="/login">Login</NavLink>&nbsp;&nbsp;&nbsp;
+      <button onClick={onLogout}>LogOut</button>
+      <Route path="/friends" component={Friends} />
+      <Route path="/login" component={Login} />
+      <Route path="/addfriend" component={AddFriendForm} />
+    </div>
   );
-  
-
-     
 }
 
 export default App;
