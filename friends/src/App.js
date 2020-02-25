efbc385a16a1cc91185f00b0e0f5a496d3a1bf3a
Alison Law <alisparklaw@gmail.com>
2020-02-25 11:55:44 +0000

    basic app set up

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Route, NavLink } from "react-router-dom";
+import Friends from "./components/Friends";
+import Login from "./components/Login";
+import AddFriendForm from "./components/AddFriend";
 
 
 
@@ -8,12 +12,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">     
-        <a className="App-link">
-          Friends
-        </a>
-      </header><br/>
       
+          Friends <br/>
+          <NavLink to="/friends">Friends</NavLink>&nbsp;&nbsp;&nbsp;
+          <NavLink to="/addfriend">Add a Friend</NavLink>&nbsp;&nbsp;&nbsp;
+          <NavLink to="/login">Login</NavLink>
+          <Route path="/friends" component={Friends} />
+          <Route path="/login" component={Login} />
+          <Route path="/addfriend" component={AddFriendForm } />
+
          </div>
      
 
